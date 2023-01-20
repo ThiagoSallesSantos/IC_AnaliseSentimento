974089476af443a62db9afc96ac7333246d523bd
ThiagoSallesSantos <thiagosallessantos3@gmail.com>
2023-01-20 14:28:45 -0300

    Refatoração modelo.py

--- src/Modelo.py
+++ src/Modelo.py
@@ -37,12 +37,10 @@ def busca_GPU() -> list:
 
 def verifica_GPU() -> None:
     try:
-        print("Iniciado Verificação da GPU")
         lista_gpu = busca_GPU()
         assert lista_gpu
         for gpu in lista_gpu:
             tf.config.experimental.set_memory_growth(gpu, True)
-        print("Finalizado Verificação da GPU")
     except AssertionError as e:
         print(f"Erro: GPU não foi encontrada: {e}")
         exit()
@@ -50,40 +48,29 @@ def verifica_GPU() -> None:
         print(f"Erro ao verificar a GPU: {e}")
         exit()
 
-def ler_datasets(dir: str, dir_dataset: str, tipo: str = ".json") -> dict[str, Dataset]:
+def ler_datasets(dir: str, arquivo_nome: str) -> Dataset:
     try:
-        print("Iniciado a Leitura dos Arquivos")
-        dict_datasets : dict[str, Dataset] = dict({})
-        for arquivo_nome in list(filter(lambda x: x.endswith(tipo), os.listdir(dir+dir_dataset))):
-            dict_datasets.update(dict({arquivo_nome: Dataset.from_json(dir+dir_dataset+arquivo_nome)}))
-        print("Finalizado a Leitura dos Arquivos")
-        return dict_datasets
+        return Dataset.from_json(dir)
     except Exception as e:
         print(f"Erro ao ler os datasets: {e}")
         exit()
 
-def get_num_k_fold(dict_datasets: dict[str, Dataset]) -> int:
-    return len(set(list(dict_datasets.values())[0]["group"]))
+def get_num_k_fold(dataset: dict[str, Dataset]) -> int:
+    return len(set(dataset["group"]))
 
-def get_num_class(dict_datasets: dict[str, Dataset]) -> int:
-    return len(list(dict_datasets.values())[0]["labels"][0])
+def get_num_class(dataset: dict[str, Dataset]) -> int:
+    return len(dataset["labels"][0])
 
 def get_tokenizer(model_id: str):
     try:
-        print("Iniciado a Obtenção do Tokenizador")
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        print("Finalizado a Obtenção do Tokenizador")
-        return tokenizer
+        return AutoTokenizer.from_pretrained(model_id)
     except Exception as e:
         print(f"Erro ao pegar o tokenizer: {e}")
         exit()
 
 def get_modelo(model_id: str, num_labels: int):
     try:
-        print("Iniciado a Obtenção do Modelo")
-        model = TFAutoModelForSequenceClassification.from_pretrained(model_id, num_labels=num_labels)
-        print("Finalizado a Obtenção do Modelo")
-        return model
+        return TFAutoModelForSequenceClassification.from_pretrained(model_id, num_labels=num_labels)
     except Exception as e:
         print(f"Erro ao pegar o modelo: {e}")
         exit()
@@ -91,133 +78,136 @@ def get_modelo(model_id: str, num_labels: int):
 def tokenize_dataset(data, **kw_args):
     return kw_args["tokenizer"](data["text"], padding=True, return_tensors="tf")
 
-def tokenizador(dict_datasets: dict[str, Dataset], tokenizer) -> dict[str, Dataset]:
+def tokenizador(dataset: Dataset, tokenizer) -> Dataset:
     try:
-        print("Iniciado a Tokenização")
-        kw_args = dict({"tokenizer": tokenizer})
-        for dataset_nome, dataset in dict_datasets.items():
-            print(f"\tIniciado a Tokenização do dataset {dataset_nome}")
-            dict_datasets[dataset_nome] = dataset.map(tokenize_dataset, batched=True, fn_kwargs=kw_args)
-            print(f"\tFinalizado a Tokenização do dataset {dataset_nome}")
-        print("Finalizado a Tokenização")
-        return dict_datasets
+        dataset = dataset.map(tokenize_dataset, batched=True, fn_kwargs=dict({"tokenizer": tokenizer}))
+        return dataset
     except Exception as e:
         print(f"Erro realizar a tokenização: {e}")
         exit()
 
-def group_by_aux(dataset: Dataset):
+def group_by(dataset: Dataset) -> list[Dataset]:
     lista_grupos = []
     for grupo in set(dataset["group"]):
         lista_grupos.append(dataset.filter(lambda x: x["group"]==grupo))
     return lista_grupos
 
-def group_by(dict_datasets: dict[str, Dataset]) -> dict[str, list[Dataset]]:
-    try:
-        print("Iniciado o Agrupamento dos Grupos")
-        for dataset_nome, dataset in dict_datasets.items():
-            print(f"\tIniciado o Agrupamento do dataset {dataset_nome}")
-            dict_datasets[dataset_nome] = group_by(dataset)
-            print(f"\tFinalizado o Agrupamento do dataset {dataset_nome}")
-        print("Finalizado o Agrupamento dos Grupos")
-        return dict_datasets
-    except Exception as e:
-        print(f"Erro ao agrupar os grupos dos datasets: {e}")
-        exit()
-
 def cria_otimizador(num_train_steps: int):
     try:
-        print("\tFinalizado a Criação do Otimizador")
         optimizer, _ = create_optimizer(
             init_lr=2e-5,
             num_train_steps=num_train_steps,
             weight_decay_rate=0.01,
             num_warmup_steps=0,
         )
-        print("\tFinalizado a Criação do Otimizador")
         return optimizer
     except Exception as e:
         print(f"Erro ao criar o otimizador: {e}")
         exit()
 
-def treinamento_aux(model, tokenizer, list_dataset: list[Dataset], optimizer, epochs: int, batchs: int) -> dict[str, int]:
-    resultado = []
-    for index, dataset in enumerate(list_dataset):
-        ## Dividindo a parte de Treino
-        dataset_treino = list_dataset[:index]
-        dataset_treino += list_dataset[index+1:]
-        dataset_treino = concatenate_datasets(dataset_treino)
-        tf_dataset_treino = model.prepare_tf_dataset(dataset_treino, batch_size=batchs, shuffle=True, tokenizer=tokenizer)
-        ##############################
-        ## Dividindo a parte de Teste
-        tf_dataset_teste = model.prepare_tf_dataset(dataset, batch_size=batchs, shuffle=True, tokenizer=tokenizer)
-        #############################
-        ## Compilando o Modelo
-        model.compile(
-            optimizer=optimizer,
-            loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-            metrics=[
-                tf.keras.metrics.CategoricalAccuracy(),
-                func_precision,
-                func_recall,
-                func_f1
-            ],
-            run_eagerly = True
-        )
-        ######################
-        ## Treinando o Modelo
-        history = model.fit(tf_dataset_treino, epochs=epochs, use_multiprocessing=True)
-        #####################
-        ## Testando o Modelo
-        loss, acc, precision, recall, f1 = model.evaluate(tf_dataset_teste, use_multiprocessing=True)
-        ####################
-        resultado.append(dict({
-            "loss" : loss,
-            "accuracy" : acc,
-            "precision" : precision,
-            "recall" : recall,
-            "f1" : f1
-        }))
-    return resultado
-
-def treinamento(dir: str, dir_resultado: str, model, tokenizer, dict_datasets: dict[str, list[Dataset]], epochs: int, batchs: int, num_k_fold: int) -> None:
+def treinamento(model, tokenizer, dataset_agrupado: list[Dataset], optimizer, epochs: int, batchs: int) -> None:
     try:
-        print("Iniciado o Treinamento GERAL")
-        optimizer = cria_otimizador((num_k_fold  // batchs) * epochs)
-        for dataset_nome, list_dataset in dict_datasets.items():
-            try:
-                print(f"\tIniciado o Treinamento do Dataset {dataset_nome}")
-                resultado = treinamento_aux(model, tokenizer, list_dataset, optimizer, epochs, batchs)
-                with open(f"{dir}{dir_resultado}{dataset_nome}.json", "w") as arquivo:
-                    json.dump(resultado, arquivo, indent=4)
-                print(f"\tFinalizado o Treinamento do Dataset {dataset_nome}")
-            except Exception as e:
-                print(f"Erro no treinamento do dataset {dataset_nome}: {e}")
-                exit()
-        print("Finalizado o Treinamento GERAL")
+        resultado = []
+        for index, dataset in enumerate(dataset_agrupado):
+            ## Dividindo a parte de Treino
+            dataset_treino = dataset_agrupado[:index]
+            dataset_treino += dataset_agrupado[index+1:]
+            dataset_treino = concatenate_datasets(dataset_treino)
+            tf_dataset_treino = model.prepare_tf_dataset(dataset_treino, batch_size=batchs, shuffle=True, tokenizer=tokenizer)
+            ##############################
+            ## Dividindo a parte de Teste
+            tf_dataset_teste = model.prepare_tf_dataset(dataset, batch_size=batchs, shuffle=True, tokenizer=tokenizer)
+            #############################
+            ## Compilando o Modelo
+            model.compile(
+                optimizer=optimizer,
+                loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                metrics=[
+                    tf.keras.metrics.CategoricalAccuracy(),
+                    func_precision,
+                    func_recall,
+                    func_f1
+                ],
+                run_eagerly = True
+            )
+            ######################
+            ## Treinando o Modelo
+            history = model.fit(tf_dataset_treino, epochs=epochs, use_multiprocessing=True)
+            #####################
+            ## Testando o Modelo
+            loss, acc, precision, recall, f1 = model.evaluate(tf_dataset_teste, use_multiprocessing=True)
+            ####################
+            resultado.append(dict({
+                "loss" : loss,
+                "accuracy" : acc,
+                "precision" : precision,
+                "recall" : recall,
+                "f1" : f1
+            }))
+        return resultado
     except Exception as e:
         print(f"Erro no treinamento: {e}")
         exit()
 
 def main(dir: str, dir_dataset: str, dir_resultado: str, model_id: str, epochs: int, batchs: int) -> None:
     try:
-        print("Iniciado a execução do código!")
+        print("---Iniciado a execução do código!---")
+        ## Verificando compatibiliada com a GPU
+        print("- Verificando compatibilidade com a GPU - Inicio")
         verifica_GPU()
-        dict_datasets = ler_datasets(dir, dir_dataset)
-        num_k_fold = get_num_k_fold(dict_datasets)
-        print(f"NUM K_FOLD: {num_k_fold}")
-        num_class = get_num_class(dict_datasets)
-        print(f"NUM CLASS: {num_class}")
-        assert False
-        tokenizer = get_tokenizer(model_id)
-        model = get_modelo(model_id, num_class)
-        dict_datasets = tokenizador(dict_datasets, tokenizer)
-        dict_datasets = group_by(dict_datasets)
-        treinamento(dir, dir_resultado, model, tokenizer, dict_datasets, epochs, batchs, num_k_fold)
-        print("Código executado com sucesso!")
-    except Exception as error:
-        print(f"Erro no main: {error}")
-        exit()
+        print("- Verificando compatibilidade com a GPU - Fim")
 
+        ## Pegando Tokenizador a ser utiizado por todos os dataset's
+        print("- Pegando o tokenizador - Inicio")
+        tokenizer = get_tokenizer(model_id)
+        print("- Pegando o tokenizador - Fim")
+
+        ## For para percorrer os datasets a serem utilizados no treinamento
+        for arquivo_nome in list(filter(lambda x: x.endswith(".json"), os.listdir(dir+dir_dataset))):
+            print(f"- Processo de treinamento usando o dataset {arquivo_nome} - Inicio")
+
+            ## Lendo Dataset
+            print(f"\t- Lendo o dataset {arquivo_nome} - Inicio")
+            dataset = ler_datasets(f"{dir}{dir_dataset}{arquivo_nome}", arquivo_nome)
+            print(f"\t- Lendo o dataset {arquivo_nome} - Fim")
+
+            ## Pegando o valor de k-fold e num_class do dataset
+            print("\t- Pegando valores de k-fold e quantidade de classes (Positivo, Negativo e Neutro) - Inicio")
+            num_k_fold = get_num_k_fold(dataset)
+            num_class = get_num_class(dataset)
+            print("\t- Pegando valores de k-fold e quantidade de classes (Positivo, Negativo e Neutro) - Fim")
+            
+            print(f"\t- Pegando o modelo para treinamento com qtd classes: {num_class} - Inicio")
+            model = get_modelo(model_id, num_class)
+            print(f"\t- Pegando o modelo para treinamento com qtd classes: {num_class} - Fim")
+
+            print(f"\t- Tokenizando os dados do dataset {arquivo_nome} - Inicio")
+            dataset = tokenizador(dataset, tokenizer)
+            print(f"\t- Tokenizando os dados do dataset {arquivo_nome} - Fim")
+
+            print(f"\t- Agrupando os dados do dataset {arquivo_nome} - Inicio")
+            dataset_agrupado = group_by(dataset)
+            print(f"\t- Agrupando os dados do dataset {arquivo_nome} - Fim")
+
+            print("\t- Criando um otimizador - Inicio")
+            optimizer = cria_otimizador((num_k_fold  // batchs) * epochs)
+            print("\t- Criando um otimizador - Inicio")
+
+            print(f"\t- Treinamento do dataset {arquivo_nome} - Inicio")
+            resultado = treinamento(model, tokenizer, dataset_agrupado, optimizer, epochs, batchs)
+            print(f"\t- Treinamento do dataset {arquivo_nome} - Fim")
+
+            print(f"\t- Salvando os resultados obtidos - Inicio")
+            with open(f"{dir}{dir_resultado}{arquivo_nome}", "w") as arquivo:
+                json.dump(resultado, arquivo, indent=4)
+            print(f"\t- Salvando os resultados obtidos - Inicio")
+
+            print(f"- Processo de treinamento usando o dataset {arquivo_nome} - Inicio")
+
+        print("---Código executado com sucesso!---")
+    except Exception as e:
+        print(f"Erro no main: {e}")
+        exit()
 
 if __name__ == "__main__":
     ## Pegando valores por linha de comando
@@ -229,7 +219,7 @@ if __name__ == "__main__":
         epochs: int = 3 if not len(sys.argv) >= 6 else sys.argv[5]
         batchs: int = 16 if not len(sys.argv) >= 7 else sys.argv[6]
         main(dir, dir_dataset, dir_resultado, model_id, epochs, batchs)
-    except Exception as error:
-        print(f"Error ao pegar as informações passadas por linha de comando: {error}")
+    except Exception as e:
+        print(f"Error ao pegar as informações passadas por linha de comando: {e}")
         exit()
 
